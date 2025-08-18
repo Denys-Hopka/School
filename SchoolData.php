8b5841bc71eae2f7dc52a898c456d31c606120e0
Denys Hopka <denyshopka@gmail.com>
2025-08-18 12:54:37 +0200

    Third commit

--- SchoolData.php
+++ SchoolData.php
@@ -12,54 +12,55 @@
 
         function print_classes()
         {
+            $this->print_header('Wählen Sie eine Klasse');
             $sql_req = 'SELECT designation FROM classes';
             $this->print_table($sql_req, 1, 'classes', 12, 'designation');
             // 'designation' is a field name from our database
             // 'classes' is a table name
             // 1 means that we want the table cells to contain links 
-            // 12 is the maximum number of cells in 1 row of the table
+            // 12 is the maximum number of database query rows in 1 row of the html table; below in the code this parameter is called ‘html_table_width’
         }
 
         function print_members($class)
         {
-
+            echo '<div class="members_tables">';
+                for($onset = 0; $onset < 35; $onset += 10)
+                    {
+                        $sql_req = 'SELECT first_name, last_name, is_student_representative FROM students LEFT JOIN classes ';
+                        $sql_req .= 'ON students.class_id = classes.class_id WHERE classes.designation = \'' . $class . '\'';
+                        $sql_req .= 'LIMIT ' . $onset . ', 10';
+                        $this->print_table($sql_req, 0, 'students', 1, 'first_name', 'last_name', 'is_student_representative');
+                    }
+            echo '</div>';
         }
 
 
         
-        function print_table($sql_req, $links_mode, $table, $cols, $first_field, $second_field = 0, $third_field = 0)
+        function print_table($sql_req, $links_mode, $table, $html_table_width, $first_field, $second_field = 0, $third_field = 0)
         {
-
-
-            
-
-
-
-            $this->print_header('Wählen Sie eine Klasse');
             echo '<table>';
             echo    '<tbody>';
             echo        '<tr>';
-
-            $i = 0;
-
-            foreach ($this->conn->query($sql_req) as $row)
+            $row_i = 0;
+            foreach($this->conn->query($sql_req) as $row)
             {
-                if($i == $cols)
+
+                $this->print_rows($row, $links_mode, $first_field, $second_field, $third_field);
+                $row_i++;
+
+                if($row_i == $html_table_width)
                 {
                     echo '</tr>';
                     echo '<tr>';
+                    $row_i = 0;
                 }
-
-                $this->print_rows($row, $links_mode, $first_field, $second_field = 0, $third_field = 0);
-
-                $i++;
             }
             echo        '</tr>';
-            echo    '<tbody>';
-            echo '<table>';
+            echo    '</tbody>';
+            echo '</table>';
         }
 
-        function print_rows($row, $links_mode, $first_field, $second_field = 0, $third_field = 0)
+        function print_rows($row, $links_mode, $first_field, $second_field, $third_field)
         {
 
             if($links_mode == 0)
@@ -73,32 +74,37 @@
                 {
                     echo '<td>'. $row[$third_field] . '</td>';
                 }
+
             }
 
             if($links_mode == 1)
             {
+
                 echo '<td>' . '<a href="?class=' . $row[$first_field] . '">' . $row[$first_field] . '</a></td>';
                 if($second_field)
                 {
                     echo '<td>' . '<a href="?class=' . $row[$second_field] . '">' . $row[$second_field] . '</a></td>';
                 }
-                if($third_field == 1)
+                if($third_field)
                 {
                     echo '<td>' . '<a href="?class=' . $row[$third_field] . '">' . $row[$third_field] . '</a></td>';
                 }
+
             }
 
             if($links_mode == 2)
             {
+
                 echo '<td>' . '<a href="?class=' . $row[$first_field] . '">' . $row[$first_field] . '</a></td>';
                 if($second_field)
                 {
                     echo '<td>' . '<a href="?class=' . $row[$second_field] . '">' . $row[$second_field] . '</a></td>';
                 }
-                if($third_field == 1)
+                if($third_field)
                 {
                     echo '<td>' . '<a href="?class=' . $row[$third_field] . '">' . $row[$third_field] . '</a></td>';
                 }
+
             }
 
         }
@@ -110,7 +116,7 @@
 
         function print_header(string $header_text)
         {
-            echo '<h1>' . $header_text . '<h1>';
+            echo '<h1>' . $header_text . '</h1>';
         }
 
     }
